64fadff2f4c31a4e86ce3695a1bc46bcdf59a2ee
Nilesh1502 <79056346+Nilesh1502@users.noreply.github.com>
2022-05-05 12:14:41 +0530

    main.cpp inputs

--- main.cpp
+++ main.cpp
@@ -1,82 +1,90 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "sha256.h"
 using namespace std;
 
-class TreeNode{
+class TreeNode
+{
     string Hashval;
     TreeNode *left;
     TreeNode *right;
-    
+
     TreeNode(string Hashval)
     {
-        this->Hashval=Hashval;
-        left=nullptr;
-        right=nullptr;
+        this->Hashval = Hashval;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
 int main()
 {
-    int i,j;
+    int i, j, n = 5;
     SHA256 sha256;
     vector<string> v;
     string s;
-    v.push_back("abc");
-    v.push_back("ijk");
-    v.push_back("xyz");
-    v.push_back("lmn");
-    v.push_back("uvw");
-    
-    
+    string input;
+    cout << "No. of strings" << endl;
+    cin >> n;
+
+    cout << "Enter strings..." << endl;
+    for (i = 0; i < n; i++)
+    {
+        cin >> input;
+        v.push_back(input);
+    }
+    // v.push_back("abc");
+    // v.push_back("ijk");
+    // v.push_back("xyz");
+    // v.push_back("lmn");
+    // v.push_back("uvw");
+
     vector<vector<string>> hashes;
     vector<string> temp;
     hashes.push_back(temp);
-    for(int i=0;i<v.size();i++)
+    for (int i = 0; i < v.size(); i++)
     {
         hashes[0].push_back(sha256(v[i]));
     }
-    int prev_nodes=v.size();
-    int flag=1;
-    int level=1;
+    int prev_nodes = v.size();
+    int flag = 1;
+    int level = 1;
 
-    while(flag)
+    while (flag)
     {
-        cout<<level<<" B"<<endl;
-        int count=0;
+        int count = 0;
         temp.clear();
         hashes.push_back(temp);
-        for(i=0;i<prev_nodes;i=i+2)
+        for (i = 0; i < prev_nodes; i = i + 2)
         {
-            string s=hashes[level-1][i];
-            if(i+1==prev_nodes)
+            string s = hashes[level - 1][i];
+            if (i + 1 == prev_nodes)
             {
-                s+=s;
+                s += s;
             }
             else
             {
-                s+=hashes[level-1][i+1];
+                s += hashes[level - 1][i + 1];
             }
-            int t=i/2;
+            int t = i / 2;
             SHA256 hashfunc;
-            //Note******
-            hashes[level][t]=hashfunc(s);
+            hashes[level].push_back(hashfunc(s));
             count++;
         }
-        prev_nodes=count;
+        prev_nodes = count;
         level++;
-        if(prev_nodes==1)
+        if (prev_nodes == 1)
         {
-            flag=0;
+            flag = 0;
         }
     }
-    
-    for(i=0;i<hashes.size();i++)
+
+    for (i = 0; i < hashes.size(); i++)
     {
-        for(j=0;j<hashes[0].size();j++)
+        for (j = 0; j < hashes[i].size(); j++)
         {
-            cout<<hashes[i][j]<<" ";
+            cout << hashes[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
